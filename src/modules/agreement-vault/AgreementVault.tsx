a2a18b03e322e74cea179ce96ed5790eaedb8078
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-18 14:58:30 -0600

    Updated AgreementVault.tsx

--- src/modules/agreement-vault/AgreementVault.tsx
+++ src/modules/agreement-vault/AgreementVault.tsx
@@ -4,3 +4,29 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Badge } from '@/components/ui/badge'
+
+function AgreementVault() {
+  return (
+    <Routes>
+      <Route path="/" element={
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Agreement Vault</CardTitle>
+              <CardDescription>Manage your agreements</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <Input
+                  placeholder="Search agreements..."
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      } />
+    </Routes>
+  )
+}
+
+export default AgreementVault
