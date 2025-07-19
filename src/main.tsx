1067716228c5b06aa48027d14b78eabd6b27dd0b
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-19 14:23:47 -0600

    Fix PDF.js worker loading error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as pdfjsLib from 'pdfjs-dist'
 import App from './App.tsx'
 import './index.css'
+
+// Configure PDF.js worker
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
