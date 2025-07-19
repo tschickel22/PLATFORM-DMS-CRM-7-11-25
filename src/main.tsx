facd14190505b60b9582e294dc851cd1abfaeea7
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-19 14:36:41 -0600

    Fix PDF.js worker import to use ?url for correct URL path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { pdfjs } from 'pdfjs-dist/legacy/build/pdf'
-import workerSrc from 'pdfjs-dist/legacy/build/pdf.worker.entry'
+import workerSrc from 'pdfjs-dist/legacy/build/pdf.worker.entry?url'
 import App from './App.tsx'
 import './index.css'
 
