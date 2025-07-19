d4adcc414ec4584a6d8b10d43ea8a806f8d26e32
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-19 14:26:50 -0600

    Fix PDF.js to use local worker instead of CDN

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as pdfjsLib from 'pdfjs-dist'
+import { pdfjs } from 'pdfjs-dist'
+import pdfWorker from 'pdfjs-dist/build/pdf.worker.min.js?url'
 import App from './App.tsx'
 import './index.css'
 
 // Configure PDF.js worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.3.93/pdf.worker.min.js'
+pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
