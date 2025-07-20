867ae9ce8ba0bd5c3f45f2d3191f9c294a8e863a
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-20 08:12:54 -0600

    Fix Duplicate FileText Import Error

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,7 +5,6 @@ import { isColorLight } from '@/lib/utils'
 import { cn } from '@/lib/utils'
 import { useTenant } from '@/contexts/TenantContext'
 import { 
-  FileText, 
   DollarSign, 
   Settings, 
   ChevronDown, 
