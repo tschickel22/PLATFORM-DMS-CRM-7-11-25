be5842a7fead0162a59aad561b15b7cfe7a83b08
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-20 20:15:40 -0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as pdfjs from 'pdfjs-dist/build/pdf'
-import workerSrc from 'pdfjs-dist/build/pdf.worker.entry?url'
+import * as pdfjs from 'pdfjs-dist'
 import App from './App.tsx'
 import './index.css'
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = workerSrc
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url
+).toString()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
