9ded707dd61f9ec77acf85a4ac30fc9ab80990af
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-22 20:42:07 -0600

    Fix PDF.js worker to use CDN URL

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import * as pdfjs from 'pdfjs-dist'
-import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.js?url'
 import App from './App.tsx'
 import './index.css'
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
