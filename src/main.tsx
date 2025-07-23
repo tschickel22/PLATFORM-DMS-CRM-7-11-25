0dbd7e523638cda1fab95465876d8051966024b4
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-22 20:40:27 -0600

    Fix PDF.js worker import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import * as pdfjs from 'pdfjs-dist'
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.min.js?url'
 import App from './App.tsx'
 import './index.css'
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url
-).toString()
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
