e7bf88de47a9a1429cb9421946e07cb44b088a8c
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-25 09:27:40 -0600

    Updated InventoryManagement.tsx

--- src/modules/inventory-management/InventoryManagement.tsx
+++ src/modules/inventory-management/InventoryManagement.tsx
@@ -5,7 +5,6 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Badge } from '@/components/ui/badge'
 import { Alert, AlertDescription } from '@/components/ui/alert'
-import { Alert, AlertDescription } from '@/components/ui/alert'
 import { 
   Package, 
   Plus, 
