859527c14ca34cc238162b0d4a71059cd67fecb4
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-07-26 15:53:37 -0600

    Fix misplaced Routes closing tag in Button

--- src/modules/pdi-checklist/PDIChecklist.tsx
+++ src/modules/pdi-checklist/PDIChecklist.tsx
@@ -183,7 +183,6 @@ function PDIChecklistDashboard() {
           </div>
           <Button onClick={handleNewInspection} className="flex items-center gap-2">
             <Plus className="h-4 w-4" />
-      </Routes>
             New Inspection
           </Button>
         </div>
